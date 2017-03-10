94f824a018c34f033c0f80c7b3eaeb67733fb6bf
Fred Sena <sena.fred@gmail.com>
2017-03-10 11:05:01 -0300

    Update RefactoringTutorial.cs

--- RefactoringTutorial.cs
+++ RefactoringTutorial.cs
@@ -2,9 +2,15 @@
 //Credits:
 //http://softwareengineering.stackexchange.com/questions/186218/helper-static-methods-in-tdd
 
-//To say that they're not testable is inaccurate. They're very testable, but they're not mockable and thus they are not test-friendly. That is, every time you test a unit of code that calls this method, you have to test the method. If the method is ever broken, many tests will fail and it won't be obvious why.
+//To say that they're not testable is inaccurate. 
+//They're very testable, but they're not mockable and thus they are not test-friendly. 
+//That is, every time you test a unit of code that calls this method, you have to test the method. 
+//If the method is ever broken, many tests will fail and it won't be obvious why.
 
-//Make it a non-static class, extract an interface with all the methods in and pass your helper into every class that needs it, preferably through the constructor, and preferably using an IOC Container.
+//Make it a non-static class, 
+//extract an interface with all the methods in and pass your helper into every class that needs it, 
+//preferably through the constructor, 
+//and preferably using an IOC Container.
 
 public class FileHelper : IFileHelper
 {
