094252a7b38061881fd1a53f7a6589690b3c338a
Fred Sena <sena.fred@gmail.com>
2018-03-22 09:05:27 +0000

    Create TodoFixSomeday.cs

---
+++ TodoFixSomeday.cs
@@ -0,0 +1,5 @@
+
+var fixture = new Fixture();
+fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList().ForEach(b => fixture.Behaviors.Remove(b));
+fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
